86a8450437e9bbf744cb8a1998c76bf23ac8e3c9
Dzuchun <Infernodambik@gmail.com>
2023-06-19 15:26:01 +0300

    Added abillity to clear local storage for touchscreen users -- make two touches at least 3 seconds long to be asked if you want to do so.

--- debug_clearstorage.js
+++ debug_clearstorage.js
@@ -6,3 +6,22 @@ document.addEventListener("keydown", (e) => {
     alert("[debug]: Config cleared");
   }
 });
+
+let timer;
+document.addEventListener("touchstart", (e) => {
+  if (e.touches.length >= 2) {
+    timer = setTimeout(() => {
+      if (confirm("[debug]: Clear config?")) {
+        localStorage.removeItem("config");
+        localStorage.removeItem("questions_data");
+        localStorage.removeItem("study_data");
+        alert("[debug]: Config cleared");
+      }
+    }, 3000);
+  }
+});
+document.addEventListener("touchend", (e) => {
+  if (timer) {
+    clearTimeout(timer);
+  }
+});

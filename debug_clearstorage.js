3c6e52c546fd8ab510304f661764ed7c8a2a4a27
Dzuchun <Infernodambik@gmail.com>
2023-06-19 15:42:17 +0300

    Fixed multiple storage-clearing prompts.

--- debug_clearstorage.js
+++ debug_clearstorage.js
@@ -10,18 +10,21 @@ document.addEventListener("keydown", (e) => {
 let timer;
 document.addEventListener("touchstart", (e) => {
   if (e.touches.length >= 2) {
-    timer = setTimeout(() => {
-      if (confirm("[debug]: Clear config?")) {
-        localStorage.removeItem("config");
-        localStorage.removeItem("questions_data");
-        localStorage.removeItem("study_data");
-        alert("[debug]: Config cleared");
-      }
-    }, 3000);
+    if (!timer) {
+      timer = setTimeout(() => {
+        if (confirm("[debug]: Clear config?")) {
+          localStorage.removeItem("config");
+          localStorage.removeItem("questions_data");
+          localStorage.removeItem("study_data");
+          alert("[debug]: Config cleared");
+        }
+      }, 3000);
+    }
   }
 });
 document.addEventListener("touchend", (e) => {
   if (timer) {
     clearTimeout(timer);
+    timer = undefined;
   }
 });
